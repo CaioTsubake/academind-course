669b0af5870eab70665695833cfbb119c6b1d853
Caio Villa Silva Tsubake <caiotsubake@gmail.com>
2022-08-08 20:01:56 -0300

    Adicionados BAckdrop e Modal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import Todo  from "./components/Todo";
+import Backdrop from "./components/Backdrop";
+import Modal from "./components/Modal";
+import Todo from "./components/Todo";
 
 function App() {
   return (
     <div>
       <h1>Meus Todos</h1>
-      <Todo text="Aprender React"/>
-      <Todo text='Dominar React'/>
-      <Todo text='Assistir todo o curso de React'/>
+      <Todo text="Aprender React" />
+      <Todo text="Dominar React" />
+      <Todo text="Assistir todo o curso de React" />
+      <Backdrop />
+      <Modal />
     </div>
   );
 }
---
+++ src/components/Modal.js
@@ -0,0 +1,11 @@
+function Modal() {
+  return (
+    <div className="modal">
+      <p>Tem certeza?</p>
+      <button className="btn btn--alt">Sim</button>
+      <button className="btn">Não</button>
+    </div>
+  );
+}
+
+export default Modal;
