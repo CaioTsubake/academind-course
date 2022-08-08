200e33aa8bf223627ca69f6fa991624009b25cf4
Caio Villa Silva Tsubake <caiotsubake@gmail.com>
2022-08-08 19:41:31 -0300

    Fazendo primeiras lições do curso

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Todo  from "./components/Todo";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Meus Todos</h1>
+      <Todo text="Aprender React"/>
+      <Todo text='Dominar React'/>
+      <Todo text='Assistir todo o curso de React'/>
     </div>
   );
 }
