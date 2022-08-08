3b4d2f172c4e18438c33c5e3044d68e77d567d59
Caio Villa Silva Tsubake <caiotsubake@gmail.com>
2022-08-08 19:52:07 -0300

    Adicionado Evento Deletar
    Por enquanto, somente printa no console uma mensagem

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,14 @@
 function Todo(props) {
+  function deletar() {
+    console.log("Clicado!");
+    console.log(props.text);
+  }
+
   return (
     <div className="card">
       <h2>{props.text}</h2>
       <div className="actions">
-        <button className="btn">Deletar</button>
+        <button className="btn" onClick={deletar}>Deletar</button>
       </div>
     </div>
   );
