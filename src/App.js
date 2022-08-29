18587768de683fb1f38f94c498d6aef8067e37ca
Caio Villa Silva Tsubake <caiotsubake@gmail.com>
2022-08-29 20:23:41 -0300

    Garantido que cada rota irá mostrar sua página
    Usando Switch para garantir que uma rota mostre somente uma única página
    e 'exact' para que o path default seja mostrado somente se o path
    atual
    for exatamente '/'

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import FavoritosPagina from "./pages/Favoritos";
 import NovoMeetupPagina from "./pages/NovoMeetup";
 import TodosMeetupsPagina from "./pages/TodosMeetups";
@@ -6,15 +6,17 @@ import TodosMeetupsPagina from "./pages/TodosMeetups";
 function App() {
   return (
     <div>
-      <Route path={"/"}>
-        <TodosMeetupsPagina />
-      </Route>
-      <Route path={"/novo-meetup"}>
-        <NovoMeetupPagina />
-      </Route>
-      <Route path={"/favoritos"}>
-        <FavoritosPagina />
-      </Route>
+      <Switch>
+        <Route path={"/"} exact>
+          <TodosMeetupsPagina />
+        </Route>
+        <Route path={"/novo-meetup"}>
+          <NovoMeetupPagina />
+        </Route>
+        <Route path={"/favoritos"}>
+          <FavoritosPagina />
+        </Route>
+      </Switch>
     </div>
   );
 }
