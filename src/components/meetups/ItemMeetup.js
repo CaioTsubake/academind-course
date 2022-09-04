5bf402c0ae8fe4562062a32deb092162bdd3cfd7
Caio Villa Silva Tsubake <caiotsubake@gmail.com>
2022-09-04 00:19:55 -0300

    Estilos de css adicionados ao ItemMeetup

--- src/components/meetups/ItemMeetup.js
+++ src/components/meetups/ItemMeetup.js
@@ -1,19 +1,21 @@
+import classes from "./ItemMeetup.module.css";
+
 function ItemMeetup(props) {
-    return (
-        <li>
-            <div>
-                <img src={props.image} alt={props.title}></img>
-            </div>
-            <div>
-                <h3>{props.title}</h3>
-                <address>{props.address}</address>
-                <p>{props.description}</p>
-            </div>
-            <div>
-                <button>Favoritar</button>
-            </div>
-        </li>
-    );
+  return (
+    <li className={classes.item}>
+      <div className={classes.image}>
+        <img src={props.image} alt={props.title}></img>
+      </div>
+      <div className={classes.content}>
+        <h3>{props.title}</h3>
+        <address>{props.address}</address>
+        <p>{props.description}</p>
+      </div>
+      <div className={classes.actions}>
+        <button>Favoritar</button>
+      </div>
+    </li>
+  );
 }
 
 export default ItemMeetup;
