01207d038d69154f0f21ce2603647a519a9eb66d
Caio Villa Silva Tsubake <caiotsubake@gmail.com>
2022-09-25 17:08:07 -0300

    Erro ao adicionar Card
    Card estava sendo usando na lista de meetups inteira, não em volta
    de cada item da lista

--- src/components/meetups/ListaMeetups.js
+++ src/components/meetups/ListaMeetups.js
@@ -1,4 +1,3 @@
-import Card from "../ui/Card";
 import ItemMeetup from "./ItemMeetup";
 import classes from "./ListaMeetups.module.css";
 
@@ -7,16 +6,14 @@ function ListaMeetups(props) {
     <ul className={classes.list}>
       {props.meetups.map((meetup) => {
         return (
-          <Card>
-            <ItemMeetup
-              key={meetup.id}
-              id={meetup.id}
-              image={meetup.image}
-              title={meetup.title}
-              address={meetup.address}
-              description={meetup.description}
-            />
-          </Card>
+          <ItemMeetup
+            key={meetup.id}
+            id={meetup.id}
+            image={meetup.image}
+            title={meetup.title}
+            address={meetup.address}
+            description={meetup.description}
+          />
         );
       })}
     </ul>
