11c6155829b0b7929804669e4f12c3ccbca57087
Caio Villa Silva Tsubake <caiotsubake@gmail.com>
2022-09-25 16:36:07 -0300

    Adicionado componente Card ao projeto
    Esse componente pode ser usado como Wrapper ao redor
    de um conteúdo para dar a ele a aparência de um cartão.

--- src/components/meetups/ListaMeetups.js
+++ src/components/meetups/ListaMeetups.js
@@ -1,3 +1,4 @@
+import Card from "../ui/Card";
 import ItemMeetup from "./ItemMeetup";
 import classes from "./ListaMeetups.module.css";
 
@@ -6,14 +7,16 @@ function ListaMeetups(props) {
     <ul className={classes.list}>
       {props.meetups.map((meetup) => {
         return (
-          <ItemMeetup
-            key={meetup.id}
-            id={meetup.id}
-            image={meetup.image}
-            title={meetup.title}
-            address={meetup.address}
-            description={meetup.description}
-          />
+          <Card>
+            <ItemMeetup
+              key={meetup.id}
+              id={meetup.id}
+              image={meetup.image}
+              title={meetup.title}
+              address={meetup.address}
+              description={meetup.description}
+            />
+          </Card>
         );
       })}
     </ul>
